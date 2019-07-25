e2ee483cd3faff1d21ba8724b1a27c377fc9520f
nitinkundu <50735799+nitinkundu@users.noreply.github.com>
2019-07-26 03:11:42 +0530

    Update service.ts

--- src/app/config/service.ts
+++ src/app/config/service.ts
@@ -5,7 +5,7 @@ import { HttpClient } from '@angular/common/http';
 @Injectable()
 export class ConfigService {
   constructor(private http: HttpClient) { }
-  configUrl = 'http://ec2-18-224-182-74.us-east-2.compute.amazonaws.com:8080/Subha_Spring_Test_1/employees/';
+  configUrl = 'http://ec2-18-224-182-74.us-east-2.compute.amazonaws.com:8080/superemp/employees';
 
 getConfig() {
   return this.http.get(this.configUrl);
